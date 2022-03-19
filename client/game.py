502b0035f156b47d1274108aeff7b5931d6532e0
kohlten <alex.strole004@gmail.com>
2022-03-19 09:52:59 -0600

    work on server and cleaned up client code

--- client/game.py
+++ client/game.py
@@ -1,9 +1,60 @@
+import pygame
+import fps
+from asset_bank import ASSET_BANK
+from window import Window
+from scrollable import Scrollable
+from time import time
+
+
 class Game:
+    """
+    Encapsulates all the game data
+    """
     def __init__(self):
-        pass
+        """
+        Creates a new window and objects in the window
+        """
+        self.__window = Window([900, 900], "Mexican Train", False, False)
+        self.__running = True
+        self.__fps_counter = fps.FpsCounter()
+        self.__scroll = Scrollable(self.__window.get_screen_size(), (0, 0))
+        self.__angle = 0
 
-    def update(self):
-        pass
+    def __events(self):
+        """
+        Polls for events
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.__running = False
+            self.__scroll.events(event)
 
-    def draw(self):
-        pass
+    def __update(self):
+        """
+        Updates objects
+        """
+        self.__fps_counter.update()
+        self.__angle += 1
+
+    def __draw(self):
+        """
+        Draws objects to display
+        """
+        screen = self.__window.get_screen()
+        screen.fill((255, 255, 255))
+        image = ASSET_BANK.get_asset("1.png")
+        rotated_image, bounding_box = image.rotate_center((100, 100), self.__angle)
+        self.__scroll.blit_item(rotated_image, bounding_box)
+        self.__scroll.draw(screen)
+        self.__window.display()
+
+    def run(self):
+        """
+        While running, poll events, update, and draw.
+        """
+        while self.__running:
+            start_time = time()
+            self.__events()
+            self.__update()
+            self.__draw()
+            fps.limit_fps(start_time, 60)
--- client/main.py
+++ client/main.py
@@ -1,42 +1,16 @@
-from time import time
-
 import pygame
-
-import fps
-from window import Window
 from image import Image
-from os import getcwd
-from scrollable import Scrollable
 from asset_bank import ASSET_BANK
+from game import Game
 
 
 def main():
-    window = Window([900, 900], "Mexican Train", False, False)
+    pygame.init()
+    game = Game()
     load_assets()
-    running = True
-    fps_counter = fps.FpsCounter()
-    image = ASSET_BANK.get_asset("1.png")
-    angle = 0
-    scroll = Scrollable(window.get_screen_size(), (0, 0))
-    while running:
-        screen = window.get_screen()
-        screen.fill((255, 255, 255))
-        start_time = time()
-        fps_counter.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            scroll.events(event)
-        rotated_image, bounding_box = image.rotate_center((100, 100), angle)
-        scroll.blit_item(rotated_image, bounding_box)
-        scroll.draw(screen)
-        angle += 1
-        if angle > 360:
-            angle = 0
-        window.display()
-        fps.limit_fps(start_time, 60)
-        #print(fps_counter.get_fps())
+    game.run()
     pygame.quit()
+
 
 def load_assets():
     ASSET_BANK.save_asset("1.png", Image("assets/Light theme/1.png"))
