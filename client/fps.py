0347fd4040062cd083e383f0d11abb402b5260fa
kohlten <alex.strole004@gmail.com>
2022-03-19 08:49:55 -0600

    Added docs

--- client/fps.py
+++ client/fps.py
@@ -2,6 +2,12 @@ from time import time, sleep
 
 
 def limit_fps(start_time, target_fps):
+    """
+    Limits this single frame based off of our target fps
+    Parameters:
+        start_time (int) Time from when the frame started
+        target_fps (int) Target fps
+    """
     time_per_frame = (1000 / target_fps) / 1000
     current_time = time()
     if time() - start_time < time_per_frame:
@@ -10,12 +16,21 @@ def limit_fps(start_time, target_fps):
 
 
 class FpsCounter:
+    """
+    Counts frames per second
+    """
     def __init__(self):
+        """
+        Createa a new counter
+        """
         self.__frames = 0
         self.__fps = 0
         self.__start_time = time()
 
     def update(self):
+        """
+        Adds a frame and checks to see if a second has passed, if it has, update the fps and restart.
+        """
         self.__frames += 1
         if time() - self.__start_time >= 1.0:
             self.__fps = self.__frames
@@ -23,4 +38,7 @@ class FpsCounter:
             self.__start_time = time()
 
     def get_fps(self):
+        """
+        Returns the calulated fps. This will be zero unitl one second has passed
+        """
         return self.__fps
--- client/window.py
+++ client/window.py
@@ -2,7 +2,19 @@ import pygame
 
 
 class Window:
+    """
+    Window abstraction class.
+    """
+
     def __init__(self, size, title, fullscreen, vsync):
+        """
+        Creates a new window.
+        Parameters:
+            size (Vector) Size of the window in pixels
+            title (String) Title to show at the top
+            fullscreen (bool) Whether to be fullscreen
+            vsync (bool) Whether to use vsync
+        """
         pygame.init()
         if fullscreen:
             flags = pygame.FULLSCREEN
@@ -13,10 +25,19 @@ class Window:
         self.__display = pygame.display.set_mode(size, flags=flags, vsync=vsync)
 
     def get_screen_size(self):
+        """
+        Returns the size of the screen
+        """
         return self.__size
 
     def get_screen(self):
+        """
+        Returns a reference to the drawing window
+        """
         return self.__display
 
     def display(self):
+        """
+        Flops the internal buffer for the window and displays the frame.
+        """
         pygame.display.flip()
